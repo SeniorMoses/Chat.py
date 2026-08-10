897a6fca5be77f85de31aaf98a4a60ff000c4829
Moses Abugri <mosessenior11@gmail.com>
2026-08-10 02:28:22 +0000

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import DB_URL
+from config import DBURL
 
 engine = create_engine(
     DATABASE_URL,
